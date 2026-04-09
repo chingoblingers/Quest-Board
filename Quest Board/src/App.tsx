cacd210c858b73e842a3e7d53dcccce3f64d547a
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-04-08 23:51:02 -0500

    created test function to test supabase link is working correctly

--- Quest Board/src/App.tsx
+++ Quest Board/src/App.tsx
@@ -1,9 +1,34 @@
-
+import {useEffect, useState} from "react"
+import {supabase}from "./lib/supabaseClient"
 
 export default function App() {
+const [questsData, setQuestsData] = useState<any[]>([])
+
+useEffect(()=>{
+  fetchQuests()
+},[])
+
+async function fetchQuests(){
+  try{
+  const {data, error} = await supabase.from('quests').select('*')
+  if (error){
+    throw error
+  }else{
+    setQuestsData(data)
+    console.log(data)
+  }
+  }catch(error){
+    console.error(`unable to fetch quests due to Error: ${error}`)
+  }
+   
+}
 
   return (
-
+    <>
+    <h1> Hello </h1>
+    <pre>{JSON.stringify(questsData, null, 2)}</pre>
+    </>
+    
   )
 }
 
